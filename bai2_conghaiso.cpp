f27fd9b41f0924de94deedca559d64825cce2811
vominhthienluxury <thanhsooon1111@gmail.com>
2024-11-04 13:33:10 +0700

    Add project files.

---
+++ bai2_conghaiso.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "CHUONG TRINH TINH TONG HAI SO " << endl;
+	int so1, so2;	//khai bao bien so1 va so2 kieu so nguyen
+	int tong;
+	so1 = 3;	//gan bien so1 voi gia tri 3
+	so2 = 9;
+	tong = so1 + so2;
+	cout << "Tong hai so: " << so1 << "+" << so2 << " = " << tong;
+	return 0;
+}
