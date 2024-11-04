b12b5e2055dda3b0f91ede3feb28b10a82a5d67d
vominhthienluxury <thanhsooon1111@gmail.com>
2024-11-04 14:18:00 +0700

    su dung

--- bai2_conghaiso.cpp
+++ bai2_conghaiso.cpp
@@ -4,9 +4,12 @@ int main() {
 	cout << "CHUONG TRINH TINH TONG HAI SO " << endl;
 	int so1, so2;	//khai bao bien so1 va so2 kieu so nguyen
 	int tong;
-	so1 = 3;	//gan bien so1 voi gia tri 3
-	so2 = 9;
+	cout << "--------------\nnhap so vao\n \t \n \t |vi tri 1|" << endl;
+	cout << "nhap so1:"; cin >> so1;
+	cout << "--------------\nnhap so vao\n \t \n \t |vi tri 2|" << endl;
+	cout << "nhap so2:"; cin >> so2;
 	tong = so1 + so2;
-	cout << "Tong hai so: " << so1 << "+" << so2 << " = " << tong;
+	cout << "Tong hai so: " << so1 << "+" << so2 << " = " << tong << endl;
+	cout << "Tong hai so: " << so1 << "+" << so2 << " = " << so1 + so2;
 	return 0;
 }
